5e32c9a154078659af14cf301aeddb4a60686583
c8c36 <c8c36@unb.ca>
2024-03-18 13:38:58 -0300

    Added FeedbackEntity

--- src/main/java/com/database/entities/FeedbackEntity.java
+++ src/main/java/com/database/entities/FeedbackEntity.java
@@ -1,5 +1,61 @@
 package com.database.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "feedback")
 public class FeedbackEntity {
-   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feedback_id", nullable = false, updatable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id")
+    private AgentInformationEntity agent;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AgentInformationEntity getAgent() {
+        return agent;
+    }
+
+    public void setAgent(AgentInformationEntity agent) {
+        this.agent = agent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
