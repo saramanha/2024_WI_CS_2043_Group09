d10cc1ea8550845a67dc619d9903d9dc583244d5
c8c36 <144710558+c8c36@users.noreply.github.com>
2024-03-17 23:03:32 -0300

    Added Country entity
    This is already getting somewhat boring and repetitive, but I am getting good at typing

--- src/main/java/com/database/entities/Country.java
+++ src/main/java/com/database/entities/Country.java
@@ -1,5 +1,35 @@
 package com.database.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "county")
 public class Country {
-   
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "country_id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "country_name", nullable = false)
+    private String countryName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
 }
