7d45f75e8c26a7fa7926ad73a2f8f0c510d47de7
c8c36 <144710558+c8c36@users.noreply.github.com>
2024-03-17 23:50:23 -0300

    Added AccountType
    Quick sort is practically faster than the heap sort due to minimum amount of array swaps

--- src/main/java/com/database/entities/AccountType.java
+++ src/main/java/com/database/entities/AccountType.java
@@ -1,5 +1,56 @@
 package com.database.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account_type")
 public class AccountType {
-   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_type_id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "current_interest_rate", nullable = false)
+    private double currentInterestRate;
+
+    @Column(name = "date_effective", nullable = false)
+    private LocalDate dateEffective;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getInterestRate() {
+        return currentInterestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        currentInterestRate = interestRate;
+    }
+
+    public LocalDate getDateEffective() {
+        return dateEffective;
+    }
+
+    public void setDateEffective(LocalDate date) {
+        dateEffective = date;
+    }
 }
