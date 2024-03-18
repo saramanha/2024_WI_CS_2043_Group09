aaefcadeb9ade5683a59775100ad82d476f4230d
c8c36 <144710558+c8c36@users.noreply.github.com>
2024-03-17 22:39:36 -0300

    Document Type entity
    First prototype of document type table entity. I have no idea if I am doing this right but I am praying for the best

--- src/main/java/com/database/entities/DocumentType.java
+++ src/main/java/com/database/entities/DocumentType.java
@@ -1,5 +1,39 @@
 package com.database.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "document_type")
 public class DocumentType {
-   
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "document_type_id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
