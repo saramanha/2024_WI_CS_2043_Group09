d9f36d3e8c04beaa68444dda91c7a041e4fb7325
c8c36 <c8c36@unb.ca>
2024-03-18 13:06:05 -0300

    Added CurrencyConversionEntity
    Additionally, a currency conversion embeddable CurrencyConversionId composite key added

--- src/main/java/com/database/entities/Client_AccountJunctionEntity.java
+++ src/main/java/com/database/entities/Client_AccountJunctionEntity.java
@@ -1,5 +1,10 @@
 package com.database.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client_account_junction")
 public class Client_AccountJunctionEntity {
-   
+    
 }
