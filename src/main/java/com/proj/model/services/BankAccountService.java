982d2b59c6968b896b9586f8488cb0fa73429828
c8c36 <144710558+c8c36@users.noreply.github.com>
2024-03-20 18:17:08 -0300

    Added initial methods to Services

--- src/main/java/com/proj/model/services/BankAccountService.java
+++ src/main/java/com/proj/model/services/BankAccountService.java
@@ -1,63 +1,106 @@
 package com.proj.model.services;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.proj.model.dtos.AccountInformationDTO;
+import com.proj.model.dtos.AgentInformationDTO;
+import com.proj.model.dtos.DepositHistoryDTO;
+import com.proj.model.dtos.TransactionHistoryDTO;
+import com.proj.model.dtos.WithdrawalHistoryDTO;
+import com.proj.model.repositories.AccountInformationRepository;
+import com.proj.model.repositories.AccountTypeRepository;
+import com.proj.model.repositories.Client_AccountJunctionRepository;
+import com.proj.model.repositories.CurrencyConversionRepository;
+import com.proj.model.repositories.CurrencyRepository;
+import com.proj.model.repositories.DepositHistoryRepository;
+import com.proj.model.repositories.TransactionStatusRepository;
+import com.proj.model.repositories.TransactionTypeRepository;
+import com.proj.model.repositories.WithdrawalHistoryRepository;
 
 // Given a AccountInformationDTO. The service can operate on it doing modifications: deposits, withdrawals, deactivation, activation, 
 // and given a second account it can do a transaction (debatable to reduce complexity)
 @Service
 public class BankAccountService {
+    private AccountInformationRepository accountInformationRepo;
+    private DepositHistoryRepository depositHistoryRepo;
+    private WithdrawalHistoryRepository withdrawalHistoryRepo;
+    private Client_AccountJunctionRepository clientAccountJuncRepo;
+    private CurrencyConversionRepository currencyConversionRepo;
+    private CurrencyRepository currencyRepo;
+    private AccountTypeRepository accountTypeRepo;
+    private TransactionTypeRepository transactionTypeRepo;
+    private TransactionStatusRepository transactionStatusRepo;
 
-    public BankAccountService(AccountInformationDTO bankAccount) {
-
+    @Autowired
+    public BankAccountService(AccountInformationRepository accountInformationRepo,
+            DepositHistoryRepository depositHistoryRepo, WithdrawalHistoryRepository withdrawalHistoryRepo,
+            Client_AccountJunctionRepository clientAccountJuncRepo, CurrencyConversionRepository currencyConversionRepo,
+            CurrencyRepository currencyRepo, AccountTypeRepository accountTypeRepo,
+            TransactionTypeRepository transactionTypeRepo, TransactionStatusRepository transactionStatusRepo) {
+        this.accountInformationRepo = accountInformationRepo;
+        this.depositHistoryRepo = depositHistoryRepo;
+        this.withdrawalHistoryRepo = withdrawalHistoryRepo;
+        this.clientAccountJuncRepo = clientAccountJuncRepo;
+        this.currencyConversionRepo = currencyConversionRepo;
+        this.currencyRepo = currencyRepo;
+        this.accountTypeRepo = accountTypeRepo;
+        this.transactionTypeRepo = transactionTypeRepo;
+        this.transactionStatusRepo = transactionStatusRepo;
     }
 
-    public void deposit() {
-
+    public AccountInformationDTO deposit(Long bankAccountId, BigDecimal sum) {
+        return null;   
+    }
+    
+    public AccountInformationDTO withdrawal(Long bankAccountId, BigDecimal sum) {
+        return null;
+    }
+    
+    public AccountInformationDTO processTransaction(Long fromBankAccountId, Long toBankAccountId, BigDecimal sum) {
+        return null;
     }
 
-    public void withdrawal() {
-
+    public AccountInformationDTO addOwner(Long bankAccountId, Long clientAccountId) {
+        return null;
+    }
+    
+    public AccountInformationDTO removeOwner(Long bankAccountId, Long clientAccountId) {
+        return null;
+    }
+    
+    public List<AgentInformationDTO> getOwners(Long bankAccountId) {
+        return null;
+    }
+    
+    public AccountInformationDTO changeCurrency(Long bankAccountId, Long currencyId) {
+        return null;
+    }
+    
+    public AccountInformationDTO deactivate(Long bankAccountId) {
+        return null;
+    }
+    
+    public AccountInformationDTO activate(Long bankAccountId) {
+        return null;
     }
 
-    public void processTransaction() {
-
+    public List<TransactionHistoryDTO> getTransactionHistory(Long bankAccountId) {
+        return null;
     }
-
-    public void addOwner() {
-
+    
+    public List<DepositHistoryDTO> getDepositHistory(Long bankAccountId) {
+        return null;
     }
-
-    public void removeOwner() {
-
+    
+    public List<WithdrawalHistoryDTO> getWithdrawalsHistory(Long bankAccountId) {
+        return null;
     }
-
-    public void getOwners() {
-
+    
+    public AccountInformationDTO createBankAccount() {
+        return null;
     }
-
-    public void changeCurrency() {
-
-    }
-
-    public void deactivate() {
-
-    }
-
-    public void activate() {
-
-    }
-
-    // public void getTransactionHistory() {
-
-    // }
-
-    // public void getDepositHistory() {
-
-    // }
-
-    // public void getWithdrawalsHistory() {
-
-    // }
 }
--- src/main/java/com/proj/model/services/ClientService.java
+++ src/main/java/com/proj/model/services/ClientService.java
@@ -1,22 +1,131 @@
 package com.proj.model.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.proj.model.dtos.AddressDTO;
+import com.proj.model.dtos.DepositHistoryDTO;
+import com.proj.model.dtos.TransactionHistoryDTO;
+import com.proj.model.dtos.WithdrawalHistoryDTO;
+import com.proj.model.repositories.AccountInformationRepository;
+import com.proj.model.repositories.AddressRepository;
+import com.proj.model.repositories.AgentInformationRepository;
+import com.proj.model.repositories.BankBranchRepository;
+import com.proj.model.repositories.CityRepository;
+import com.proj.model.repositories.Client_AccountJunctionRepository;
+import com.proj.model.repositories.CountryRepository;
+import com.proj.model.repositories.CredentialRepository;
+import com.proj.model.repositories.DocumentRepository;
+import com.proj.model.repositories.DocumentTypeRepository;
+import com.proj.model.repositories.GenderRepository;
+import com.proj.model.repositories.ProvinceRepository;
+import com.proj.model.repositories.RoleRepository;
 
 // The only intended way to modify information related to a client object
 // This class updates and persists the updates made to a given client object
 @Service
 public class ClientService {
-
-    public ClientService() {};
-    public ClientService(Client client) {
-
+    private AccountInformationRepository accountInfoRepo;
+    private AgentInformationRepository agentInfoRepo;
+    private Client_AccountJunctionRepository clientBankAccountJuncRepo;
+    private AddressRepository addressRepo;
+    private CountryRepository countryRepo;
+    private ProvinceRepository provinceRepo;
+    private CityRepository cityRepo;
+    private CredentialRepository credentialRepo;
+    private BankBranchRepository bankBranchRepo;
+    private DocumentRepository docRepo;
+    private DocumentTypeRepository docTypeRepo;
+    private RoleRepository roleRepo;
+    private GenderRepository genderRepo;
+    
+    @Autowired
+    public ClientService(AccountInformationRepository accountInfoRepo, AgentInformationRepository agentInfoRepo,
+            Client_AccountJunctionRepository clientBankAccountJuncRepo, AddressRepository addressRepo,
+            CountryRepository countryRepo, ProvinceRepository provinceRepo, CityRepository cityRepo,
+            CredentialRepository credentialRepo, BankBranchRepository bankBranchRepo, DocumentRepository docRepo,
+            DocumentTypeRepository docTypeRepo, RoleRepository roleRepo, GenderRepository genderRepo) {
+        this.accountInfoRepo = accountInfoRepo;
+        this.agentInfoRepo = agentInfoRepo;
+        this.clientBankAccountJuncRepo = clientBankAccountJuncRepo;
+        this.addressRepo = addressRepo;
+        this.countryRepo = countryRepo;
+        this.provinceRepo = provinceRepo;
+        this.cityRepo = cityRepo;
+        this.credentialRepo = credentialRepo;
+        this.bankBranchRepo = bankBranchRepo;
+        this.docRepo = docRepo;
+        this.docTypeRepo = docTypeRepo;
+        this.roleRepo = roleRepo;
+        this.genderRepo = genderRepo;
     }
 
-    public Client getCurrentClient() {
+    public Client updateCredentials(Client client) {
+        return null;
+    }
+    
+    public Client updateFirstName(Client client, String newName) {
+        return null;
+    }
+    
+    public Client updateLastName(Client client, String newName) {
         return null;
     }
 
-    public void saveClient() {
+    public Client updateMiddleName(Client client, String newName) {
+        return null;
+    }
 
+    public Client updateAdditionalName(Client client, String newName, boolean doAppend) {
+        return null;
+    }
+    
+    public Client updatePrimaryAddress(Client client, AddressDTO newAddress) {
+        return null;
+    }
+    
+    public Client updateSecondaryAddress(Client client, AddressDTO newAddress) {
+        return null;
+    }
+    
+    public Client updateEmail(Client client, String email) {
+        return null;
+    }
+    
+    public Client updateGender(Client client, Long genderId) {
+        return null;
+    }
+    
+    public Client updateSex(Client client, char newSex) {
+        return null;
+    }
+    
+    public Client updateRole(Client client, Long roleId) {
+        return null;
+    }
+    
+    public Client addBankAccount(Client client, Long bankAccountId) {
+        return null;
+    }
+    
+    public List<DepositHistoryDTO> fetchDepositHistory(Client client, Integer length, Long bankAccountId) {
+        // Negative values for <bankAccountId> will fetch all entries up to <length>
+        return null;
+    }
+    
+    public List<WithdrawalHistoryDTO> fetchWithdrawalHistory(Client client, Integer length, Long bankAccountId) {
+        // Negative values for <bankAccountId> will fetch all entries up to <length>
+        return null;
+    }
+    
+    public List<TransactionHistoryDTO> fetchTransactionHistory(Client client, Integer length, Long fromBankAccountId, Long toBankAccountId) {
+        // Negative values for <bankAccountId> will fetch all entries up to <length>
+        return null;
+    }
+
+    public Client createNewClient() {
+        return null;
     }
 }
