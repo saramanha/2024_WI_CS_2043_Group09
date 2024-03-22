332491cc45c61972d9134be94c04f34265c2cf49
c8c36 <c8c36@unb.ca>
2024-03-22 14:09:57 -0300

    Fixed a query
    There is much more to come.
    However, now it runs for sure

--- src/main/java/com/proj/model/repositories/TransactionHistoryRepository.java
+++ src/main/java/com/proj/model/repositories/TransactionHistoryRepository.java
@@ -2,11 +2,16 @@ package com.proj.model.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.proj.model.embeddables.TransactionHistoryId;
 import com.proj.model.entities.TransactionHistoryEntity;
 
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistoryEntity, TransactionHistoryId>{
-    List<TransactionHistoryEntity> findAllByDepositAgentIdOrWithdrawalAgentId(Long agentId);
-    List<TransactionHistoryEntity> findAllByDepositAccountInformationIdOrWithdrawalAccountInformationId(Long accountInformationId);
+    @Query("SELECT t FROM TransactionHistoryEntity t WHERE t.deposit.agent.id = :agentId OR t.withdrawal.agent.id = :agentId")
+    List<TransactionHistoryEntity> findAllByDepositAgentIdOrWithdrawalAgentId(@Param("agentId") Long agentId);
+
+    @Query("SELECT t FROM TransactionHistoryEntity t WHERE t.deposit.accountInformation.id = :accountInformationId OR t.withdrawal.accountInformation.id = :accountInformationId")
+    List<TransactionHistoryEntity> findAllByDepositAccountInformationIdOrWithdrawalAccountInformationId(@Param("accountInformationId") Long accountInformationId);
 }
