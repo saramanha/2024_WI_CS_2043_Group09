a8246620998417f9a39a833f9aed1e965a35e3c3
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:22:16 -0300

    Update DocumentTypeEntity.java

--- src/main/java/com/proj/model/entities/DocumentTypeEntity.java
+++ src/main/java/com/proj/model/entities/DocumentTypeEntity.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Represents the entity mapping for document types.
+ */
 @Entity
 @Table(name = "document_type")
 public class DocumentTypeEntity {
@@ -18,26 +21,55 @@ public class DocumentTypeEntity {
     @Column(name = "description", nullable = false)
     private String description;
 
+    /**
+     * Constructs a document type entity with the provided parameters.
+     * 
+     * @param id           The document type ID.
+     * @param description  The description of the document type.
+     */
     public DocumentTypeEntity(Long id, String description) {
         this.id = id;
         this.description = description;
     }
 
+    /**
+     * Default constructor.
+     */
     public DocumentTypeEntity() {
     }
 
+    /**
+     * Retrieves the ID of the document type.
+     * 
+     * @return The document type ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the document type.
+     * 
+     * @param id The document type ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the description of the document type.
+     * 
+     * @return The description of the document type.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the document type.
+     * 
+     * @param description The description to set.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
