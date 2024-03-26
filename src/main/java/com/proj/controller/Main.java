c43ca1dcd7c6afad82c403ec8e1ebe373c7b69a7
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 11:52:23 -0300

    documented Main.java

--- src/main/java/com/proj/controller/Main.java
+++ src/main/java/com/proj/controller/Main.java
@@ -6,14 +6,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * The Main class serves as the entry point for the Spring Boot application.
+ * It initializes the Spring Boot environment and starts the application.
+ */
 @SpringBootApplication
 @EntityScan("com.proj.model.entities")
 @EnableJpaRepositories("com.proj.model.repositories")
 public class Main implements CommandLineRunner {
+
+    /**
+     * The main method starts the Spring Boot application.
+     * 
+     * @param args Command-line arguments passed to the application.
+     */
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
     
+    /**
+     * This method is invoked after the application context has been loaded.
+     * It does not perform any specific tasks in this implementation.
+     * 
+     * @param args Command-line arguments passed to the application.
+     */
     @Override
     public void run(String... args) {
         return;
