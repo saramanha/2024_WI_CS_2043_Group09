d2639a5b9eddc1de6b39ae98c1e734df19032453
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:37:21 -0300

    Update WithdrawalHistoryEntity.java

--- src/main/java/com/proj/model/entities/WithdrawalHistoryEntity.java
+++ src/main/java/com/proj/model/entities/WithdrawalHistoryEntity.java
@@ -46,6 +46,18 @@ public class WithdrawalHistoryEntity {
     @Column(name = "date_time_initiated")
     private LocalDateTime dateTimeInitiated;
 
+    /**
+     * Constructs a new WithdrawalHistoryEntity object with the given parameters.
+     * 
+     * @param id                    The id of the withdrawal history.
+     * @param agent                 The agent associated with the withdrawal.
+     * @param accountInformation    The account information associated with the withdrawal.
+     * @param transactionStatus     The status of the withdrawal transaction.
+     * @param sumInitial            The initial sum of the withdrawal.
+     * @param withdrawalCurrency    The currency used for withdrawal.
+     * @param sumConverted          The converted sum of the withdrawal.
+     * @param dateTimeInitiated     The date and time when the withdrawal was initiated.
+     */
     public WithdrawalHistoryEntity(Long id, AgentInformationEntity agent, AccountInformationEntity accountInformation,
             TransactionStatusEntity transactionStatus, BigDecimal sumInitial, CurrencyEntity withdrawalCurrency,
             BigDecimal sumConverted, LocalDateTime dateTimeInitiated) {
@@ -59,69 +71,152 @@ public class WithdrawalHistoryEntity {
         this.dateTimeInitiated = dateTimeInitiated;
     }
 
+    /**
+     * Default constructor for the WithdrawalHistoryEntity class.
+     */
     public WithdrawalHistoryEntity() {
     }
 
+    /**
+     * Retrieves the ID of the withdrawal history.
+     * 
+     * @return The ID of the withdrawal history.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the withdrawal history.
+     * 
+     * @param id The ID of the withdrawal history.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the agent associated with the withdrawal.
+     * 
+     * @return The agent associated with the withdrawal.
+     */
     public AgentInformationEntity getAgent() {
         return agent;
     }
 
+    /**
+     * Sets the agent associated with the withdrawal.
+     * 
+     * @param agent The agent associated with the withdrawal.
+     */
     public void setAgent(AgentInformationEntity agent) {
         this.agent = agent;
     }
 
+    /**
+     * Retrieves the account information associated with the withdrawal.
+     * 
+     * @return The account information associated with the withdrawal.
+     */
     public AccountInformationEntity getAccountInformation() {
         return accountInformation;
     }
 
+    /**
+     * Sets the account information associated with the withdrawal.
+     * 
+     * @param accountInformation The account information associated with the withdrawal.
+     */
     public void setAccountInformation(AccountInformationEntity accountInformation) {
         this.accountInformation = accountInformation;
     }
 
+    /**
+     * Retrieves the transaction status of the withdrawal.
+     * 
+     * @return The transaction status of the withdrawal.
+     */
     public TransactionStatusEntity getTransactionStatus() {
         return transactionStatus;
     }
 
+    /**
+     * Sets the transaction status of the withdrawal.
+     * 
+     * @param transactionStatus The transaction status of the withdrawal.
+     */
     public void setTransactionStatus(TransactionStatusEntity transactionStatus) {
         this.transactionStatus = transactionStatus;
     }
 
+    /**
+     * Retrieves the initial sum of the withdrawal.
+     * 
+     * @return The initial sum of the withdrawal.
+     */
     public BigDecimal getSumInitial() {
         return sumInitial;
     }
 
+    /**
+     * Sets the initial sum of the withdrawal.
+     * 
+     * @param sumInitial The initial sum of the withdrawal.
+     */
     public void setSumInitial(BigDecimal sumInitial) {
         this.sumInitial = sumInitial;
     }
 
-    public CurrencyEntity getDepositCurrency() {
+    /**
+     * Retrieves the currency used for withdrawal.
+     * 
+     * @return The currency used for withdrawal.
+     */
+    public CurrencyEntity getWithdrawalCurrency() {
         return withdrawalCurrency;
     }
 
-    public void setDepositCurrency(CurrencyEntity withdrawalCurrency) {
+    /**
+     * Sets the currency used for withdrawal.
+     * 
+     * @param withdrawalCurrency The currency used for withdrawal.
+     */
+    public void setWithdrawalCurrency(CurrencyEntity withdrawalCurrency) {
         this.withdrawalCurrency = withdrawalCurrency;
     }
 
+    /**
+     * Retrieves the converted sum of the withdrawal.
+     * 
+     * @return The converted sum of the withdrawal.
+     */
     public BigDecimal getSumConverted() {
         return sumConverted;
     }
 
+    /**
+     * Sets the converted sum of the withdrawal.
+     * 
+     * @param sumConverted The converted sum of the withdrawal.
+     */
     public void setSumConverted(BigDecimal sumConverted) {
         this.sumConverted = sumConverted;
     }
 
+    /**
+     * Retrieves the date and time when the withdrawal was initiated.
+     * 
+     * @return The date and time when the withdrawal was initiated.
+     */
     public LocalDateTime getDateTimeInitiated() {
         return dateTimeInitiated;
     }
 
+    /**
+     * Sets the date and time when the withdrawal was initiated.
+     * 
+     * @param dateTimeInitiated The date and time when the withdrawal was initiated.
+     */
     public void setDateTimeInitiated(LocalDateTime dateTimeInitiated) {
         this.dateTimeInitiated = dateTimeInitiated;
     }
