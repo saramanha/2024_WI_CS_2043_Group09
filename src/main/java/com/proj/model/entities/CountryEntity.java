ad95359d5a0ef11c8bc0a7f26733aad18f8ceece
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:14:33 -0300

    document CountryEntity.java

--- src/main/java/com/proj/model/entities/CountryEntity.java
+++ src/main/java/com/proj/model/entities/CountryEntity.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Represents the entity mapping for a country.
+ */
 @Entity
 @Table(name = "county")
 public class CountryEntity {
@@ -18,26 +21,55 @@ public class CountryEntity {
     @Column(name = "country_name", nullable = false)
     private String countryName;
 
+    /**
+     * Constructs a country entity with the provided parameters.
+     * 
+     * @param id          The ID of the country.
+     * @param countryName The name of the country.
+     */
     public CountryEntity(Long id, String countryName) {
         this.id = id;
         this.countryName = countryName;
     }
 
+    /**
+     * Default constructor.
+     */
     public CountryEntity() {
     }
 
+    /**
+     * Retrieves the ID of the country.
+     * 
+     * @return The ID of the country.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the country.
+     * 
+     * @param id The ID to set for the country.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the name of the country.
+     * 
+     * @return The name of the country.
+     */
     public String getCountryName() {
         return countryName;
     }
 
+    /**
+     * Sets the name of the country.
+     * 
+     * @param countryName The name to set for the country.
+     */
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
