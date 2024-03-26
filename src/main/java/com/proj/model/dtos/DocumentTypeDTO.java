5a299f309136784e8b2f32f5f15b170a02bfd256
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 13:18:17 -0300

    documented DocumentTypeDTO.java

--- src/main/java/com/proj/model/dtos/DocumentTypeDTO.java
+++ src/main/java/com/proj/model/dtos/DocumentTypeDTO.java
@@ -1,36 +1,61 @@
 package com.proj.model.dtos;
 
-
-
+/**
+ * Represents a DTO (Data Transfer Object) for a document type.
+ */
 public class DocumentTypeDTO {
-    
-    
-    
-    private Long id;
+    private Long id; // The ID of the document type.
+    private String description;  // The description of the document type.
 
-    
-    private String description;
-
+    /**
+     * Constructs a DocumentTypeDTO object with the specified parameters.
+     *
+     * @param id          The ID of the document type.
+     * @param description The description of the document type.
+     */
     public DocumentTypeDTO(Long id, String description) {
         this.id = id;
         this.description = description;
     }
 
+    /**
+     * Constructs an empty DocumentTypeDTO object.
+     */
     public DocumentTypeDTO() {
     }
 
+    /**
+     * Retrieves the ID of the document type.
+     *
+     * @return The ID of the document type.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the document type.
+     *
+     * @param id The ID of the document type.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the description of the document type.
+     *
+     * @return The description of the document type.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the document type.
+     *
+     * @param description The description of the document type.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
