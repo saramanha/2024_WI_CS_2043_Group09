b53fb0b4cc4cc91e00d8babff7fb4395792f249a
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:32:37 -0300

    Update TransactionHistoryEntity.java

--- src/main/java/com/proj/model/entities/TransactionHistoryEntity.java
+++ src/main/java/com/proj/model/entities/TransactionHistoryEntity.java
@@ -13,6 +13,9 @@ import javax.persistence.CascadeType;
 
 import com.proj.model.embeddables.TransactionHistoryId;
 
+/**
+ * Represents the entity mapping for transaction history.
+ */
 @Entity
 @Table(name = "transaction_history")
 public class TransactionHistoryEntity {
@@ -43,6 +46,17 @@ public class TransactionHistoryEntity {
     @Column(name = "transaction_closure_date_time")
     private LocalDateTime transactionClosureDateTime;
 
+    /**
+     * Constructs a transaction history entity with the provided parameters.
+     * 
+     * @param id                           The transaction history ID.
+     * @param deposit                      The associated deposit history entity.
+     * @param withdrawal                   The associated withdrawal history entity.
+     * @param transactionType              The associated transaction type entity.
+     * @param transactionStatus            The associated transaction status entity.
+     * @param transactionInitiationDateTime The initiation date and time of the transaction.
+     * @param transactionClosureDateTime   The closure date and time of the transaction.
+     */
     public TransactionHistoryEntity(TransactionHistoryId id, DepositHistoryEntity deposit,
             WithdrawalHistoryEntity withdrawal, TransactionTypeEntity transactionType,
             TransactionStatusEntity transactionStatus, LocalDateTime transactionInitiationDateTime,
@@ -56,61 +70,136 @@ public class TransactionHistoryEntity {
         this.transactionClosureDateTime = transactionClosureDateTime;
     }
 
+    /**
+     * Default constructor.
+     */
     public TransactionHistoryEntity() {
     }
 
+    // Getters and setters
+
+    /**
+     * Retrieves the ID of the transaction history.
+     * 
+     * @return The transaction history ID.
+     */
     public TransactionHistoryId getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the transaction history.
+     * 
+     * @param id The transaction history ID to set.
+     */
     public void setId(TransactionHistoryId id) {
         this.id = id;
     }
 
+       /**
+     * Retrieves the associated deposit history entity.
+     * 
+     * @return The associated deposit history entity.
+     */
     public DepositHistoryEntity getDeposit() {
         return deposit;
     }
 
+    /**
+     * Sets the associated deposit history entity.
+     * 
+     * @param deposit The deposit history entity to set.
+     */
     public void setDeposit(DepositHistoryEntity deposit) {
         this.deposit = deposit;
     }
 
+    /**
+     * Retrieves the associated withdrawal history entity.
+     * 
+     * @return The associated withdrawal history entity.
+     */
     public WithdrawalHistoryEntity getWithdrawal() {
         return withdrawal;
     }
 
+    /**
+     * Sets the associated withdrawal history entity.
+     * 
+     * @param withdrawal The withdrawal history entity to set.
+     */
     public void setWithdrawal(WithdrawalHistoryEntity withdrawal) {
         this.withdrawal = withdrawal;
     }
 
+    /**
+     * Retrieves the associated transaction type entity.
+     * 
+     * @return The associated transaction type entity.
+     */
     public TransactionTypeEntity getTransactionType() {
         return transactionType;
     }
 
+    /**
+     * Sets the associated transaction type entity.
+     * 
+     * @param transactionType The transaction type entity to set.
+     */
     public void setTransactionType(TransactionTypeEntity transactionType) {
         this.transactionType = transactionType;
     }
 
+    /**
+     * Retrieves the associated transaction status entity.
+     * 
+     * @return The associated transaction status entity.
+     */
     public TransactionStatusEntity getTransactionStatus() {
         return transactionStatus;
     }
 
+    /**
+     * Sets the associated transaction status entity.
+     * 
+     * @param transactionStatus The transaction status entity to set.
+     */
     public void setTransactionStatus(TransactionStatusEntity transactionStatus) {
         this.transactionStatus = transactionStatus;
     }
 
+    /**
+     * Retrieves the initiation date and time of the transaction.
+     * 
+     * @return The initiation date and time of the transaction.
+     */
     public LocalDateTime getTransactionInitiationDateTime() {
         return transactionInitiationDateTime;
     }
 
+    /**
+     * Sets the initiation date and time of the transaction.
+     * 
+     * @param transactionInitiationDateTime The initiation date and time to set.
+     */
     public void setTransactionInitiationDateTime(LocalDateTime transactionInitiationDateTime) {
         this.transactionInitiationDateTime = transactionInitiationDateTime;
     }
 
+    /**
+     * Retrieves the closure date and time of the transaction.
+     * 
+     * @return The closure date and time of the transaction.
+     */
     public LocalDateTime getTransactionClosureDateTime() {
         return transactionClosureDateTime;
     }
 
+    /**
+     * Sets the closure date and time of the transaction.
+     * 
+     * @param transactionClosureDateTime The closure date and time to set.
+     */
     public void setTransactionClosureDateTime(LocalDateTime transactionClosureDateTime) {
         this.transactionClosureDateTime = transactionClosureDateTime;
     }
