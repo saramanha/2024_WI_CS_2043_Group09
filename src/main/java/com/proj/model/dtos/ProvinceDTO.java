3218ac632fd283f14406649bdc8547961bfd738a
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 13:32:25 -0300

    documented ProvinceDTO.java

--- src/main/java/com/proj/model/dtos/ProvinceDTO.java
+++ src/main/java/com/proj/model/dtos/ProvinceDTO.java
@@ -1,34 +1,56 @@
 package com.proj.model.dtos;
 
+/**
+ * A Data Transfer Object (DTO) representing province information.
+ */
 public class ProvinceDTO {
-    
-    
-    
-    private Long id;
+    private Long id; // The unique identifier of the province.
+    private String provinceName; // The name of the province.
 
-    
-    private String provinceName;
-
+    /**
+     * Constructs a new ProvinceDTO object with specified parameters.
+     * @param id The unique identifier of the province.
+     * @param provinceName The name of the province.
+     */
     public ProvinceDTO(Long id, String provinceName) {
         this.id = id;
         this.provinceName = provinceName;
     }
 
+    /**
+     * Default constructor for ProvinceDTO.
+     */
     public ProvinceDTO() {
     }
 
+    /**
+     * Retrieves the ID of the province.
+     * @return The ID of the province.
+     */
     public Long getId() {
         return id;
-    } 
+    }
 
+    /**
+     * Sets the ID of the province.
+     * @param id The ID of the province.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the name of the province.
+     * @return The name of the province.
+     */
     public String getProvinceName() {
         return provinceName;
     }
 
+    /**
+     * Sets the name of the province.
+     * @param provinceName The name of the province.
+     */
     public void setProvinceName(String provinceName) {
         this.provinceName = provinceName;
     }
