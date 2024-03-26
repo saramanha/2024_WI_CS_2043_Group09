b220437a58da1cbb886ae53a0d24b4621f705f88
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:17:04 -0300

    Update CurrencyConversionEntity.java

--- src/main/java/com/proj/model/entities/CurrencyConversionEntity.java
+++ src/main/java/com/proj/model/entities/CurrencyConversionEntity.java
@@ -13,6 +13,9 @@ import javax.persistence.CascadeType;
 
 import com.proj.model.embeddables.CurrencyConversionId;
 
+/**
+ * Represents the entity mapping for currency conversion details.
+ */
 @Entity
 @Table(name = "currency_conversion")
 public class CurrencyConversionEntity {
@@ -38,6 +41,16 @@ public class CurrencyConversionEntity {
     @Column(name = "effective_date_time")
     private LocalDateTime effectiveDateTime;
 
+    /**
+     * Constructs a currency conversion entity with the provided parameters.
+     * 
+     * @param id                  The currency conversion ID.
+     * @param fromCurrency        The source currency.
+     * @param toCurrency          The target currency.
+     * @param multRateDenominator The denominator for the conversion rate.
+     * @param multRateNumerator   The numerator for the conversion rate.
+     * @param effectiveDateTime   The effective date and time of the conversion rate.
+     */
     public CurrencyConversionEntity(CurrencyConversionId id, CurrencyEntity fromCurrency, CurrencyEntity toCurrency,
             Long multRateDenominator, Long multRateNumerator, LocalDateTime effectiveDateTime) {
         this.id = id;
@@ -48,53 +61,116 @@ public class CurrencyConversionEntity {
         this.effectiveDateTime = effectiveDateTime;
     }
 
+    /**
+     * Default constructor.
+     */
     public CurrencyConversionEntity() {
     }
 
+    /**
+     * Retrieves the currency conversion ID.
+     * 
+     * @return The currency conversion ID.
+     */
     public CurrencyConversionId getId() {
         return id;
     }
 
+    /**
+     * Sets the currency conversion ID.
+     * 
+     * @param id The currency conversion ID to set.
+     */
     public void setId(CurrencyConversionId id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the source currency.
+     * 
+     * @return The source currency.
+     */
     public CurrencyEntity getFromCurrency() {
         return fromCurrency;
     }
 
+    /**
+     * Sets the source currency.
+     * 
+     * @param fromCurrency The source currency to set.
+     */
     public void setFromCurrency(CurrencyEntity fromCurrency) {
         this.fromCurrency = fromCurrency;
     }
 
+    /**
+     * Retrieves the target currency.
+     * 
+     * @return The target currency.
+     */
     public CurrencyEntity getToCurrency() {
         return toCurrency;
     }
 
+    /**
+     * Sets the target currency.
+     * 
+     * @param toCurrency The target currency to set.
+     */
     public void setToCurrency(CurrencyEntity toCurrency) {
         this.toCurrency = toCurrency;
     }
 
+    /**
+     * Retrieves the denominator for the conversion rate.
+     * 
+     * @return The denominator for the conversion rate.
+     */
     public Long getMultRateDenominator() {
         return multRateDenominator;
     }
 
+    /**
+     * Sets the denominator for the conversion rate.
+     * 
+     * @param multRateDenominator The denominator to set for the conversion rate.
+     */
     public void setMultRateDenominator(Long multRateDenominator) {
         this.multRateDenominator = multRateDenominator;
     }
 
+    /**
+     * Retrieves the numerator for the conversion rate.
+     * 
+     * @return The numerator for the conversion rate.
+     */
     public Long getMultRateNumerator() {
         return multRateNumerator;
     }
 
+    /**
+     * Sets the numerator for the conversion rate.
+     * 
+     * @param multRateNumerator The numerator to set for the conversion rate.
+     */
     public void setMultRateNumerator(Long multRateNumerator) {
         this.multRateNumerator = multRateNumerator;
     }
 
+    /**
+     * Retrieves the effective date and time of the conversion rate.
+     * 
+     * @return The effective date and time of the conversion rate.
+     */
     public LocalDateTime getEffectiveDateTime() {
         return effectiveDateTime;
     }
 
+    /**
+     * Sets the effective date and time of the conversion rate.
+     * 
+     * @param effectiveDateTime The effective date and time to set for the conversion rate.
+     */
     public void setEffectiveDateTime(LocalDateTime effectiveDateTime) {
         this.effectiveDateTime = effectiveDateTime;
     }
