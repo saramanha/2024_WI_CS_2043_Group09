54c732cfe6bdbf1248d444cefed355ade257270d
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:23:29 -0300

    Update DutyHistoryEntity.java

--- src/main/java/com/proj/model/entities/DutyHistoryEntity.java
+++ src/main/java/com/proj/model/entities/DutyHistoryEntity.java
@@ -10,8 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.CascadeType;
 
+/**
+ * Represents the entity mapping for duty history.
+ */
 @Entity
 @Table(name = "duty_history")
 public class DutyHistoryEntity {
@@ -26,17 +28,27 @@ public class DutyHistoryEntity {
     @Column(name = "date_time_signoff")
     private LocalDateTime dateTimeSignoff;
 
-    @ManyToOne(cascade = CascadeType.MERGE)
+    @ManyToOne
     @JoinColumn(name = "bank_branch_id")
     private BankBranchEntity bankBranch;
 
     @Column(name = "duty_notes")
     private String dutyNotes;
 
-    @ManyToOne(cascade = CascadeType.MERGE)
+    @ManyToOne
     @JoinColumn(name = "agent_id")
     private AgentInformationEntity agent;
 
+    /**
+     * Constructs a duty history entity with the provided parameters.
+     * 
+     * @param id                The duty history ID.
+     * @param dateTimeInitiated The date and time when the duty was initiated.
+     * @param dateTimeSignoff   The date and time when the duty was signed off.
+     * @param bankBranch        The bank branch associated with the duty.
+     * @param dutyNotes         Notes related to the duty.
+     * @param agent             The agent associated with the duty.
+     */
     public DutyHistoryEntity(Long id, LocalDateTime dateTimeInitiated, LocalDateTime dateTimeSignoff,
             BankBranchEntity bankBranch, String dutyNotes, AgentInformationEntity agent) {
         this.id = id;
@@ -47,53 +59,116 @@ public class DutyHistoryEntity {
         this.agent = agent;
     }
 
+    /**
+     * Default constructor.
+     */
     public DutyHistoryEntity() {
     }
 
+    /**
+     * Retrieves the ID of the duty history.
+     * 
+     * @return The duty history ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the duty history.
+     * 
+     * @param id The duty history ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the date and time when the duty was initiated.
+     * 
+     * @return The date and time when the duty was initiated.
+     */
     public LocalDateTime getDateTimeInitiated() {
         return dateTimeInitiated;
     }
 
+    /**
+     * Sets the date and time when the duty was initiated.
+     * 
+     * @param dateTimeInitiated The date and time to set.
+     */
     public void setDateTimeInitiated(LocalDateTime dateTimeInitiated) {
         this.dateTimeInitiated = dateTimeInitiated;
     }
 
+    /**
+     * Retrieves the date and time when the duty was signed off.
+     * 
+     * @return The date and time when the duty was signed off.
+     */
     public LocalDateTime getDateTimeSignoff() {
         return dateTimeSignoff;
     }
 
+    /**
+     * Sets the date and time when the duty was signed off.
+     * 
+     * @param dateTimeSignoff The date and time to set.
+     */
     public void setDateTimeSignoff(LocalDateTime dateTimeSignoff) {
         this.dateTimeSignoff = dateTimeSignoff;
     }
 
+    /**
+     * Retrieves the bank branch associated with the duty.
+     * 
+     * @return The bank branch associated with the duty.
+     */
     public BankBranchEntity getBankBranch() {
         return bankBranch;
     }
 
+    /**
+     * Sets the bank branch associated with the duty.
+     * 
+     * @param bankBranch The bank branch to set.
+     */
     public void setBankBranch(BankBranchEntity bankBranch) {
         this.bankBranch = bankBranch;
     }
 
+    /**
+     * Retrieves the notes related to the duty.
+     * 
+     * @return The duty notes.
+     */
     public String getDutyNotes() {
         return dutyNotes;
     }
 
+    /**
+     * Sets the notes related to the duty.
+     * 
+     * @param dutyNotes The duty notes to set.
+     */
     public void setDutyNotes(String dutyNotes) {
         this.dutyNotes = dutyNotes;
     }
 
+    /**
+     * Retrieves the agent associated with the duty.
+     * 
+     * @return The agent associated with the duty.
+     */
     public AgentInformationEntity getAgent() {
         return agent;
     }
 
+    /**
+     * Sets the agent associated with the duty.
+     * 
+     * @param agent The agent to set.
+     */
     public void setAgent(AgentInformationEntity agent) {
         this.agent = agent;
     }
