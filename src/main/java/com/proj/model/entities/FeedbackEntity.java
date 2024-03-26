c1a1d1b2266199b3f3f8dcca1ce62b593ff2e91a
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:24:46 -0300

    Update FeedbackEntity.java

--- src/main/java/com/proj/model/entities/FeedbackEntity.java
+++ src/main/java/com/proj/model/entities/FeedbackEntity.java
@@ -8,8 +8,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.CascadeType;
 
+/**
+ * Represents the entity mapping for feedback.
+ */
 @Entity
 @Table(name = "feedback")
 public class FeedbackEntity {
@@ -18,7 +20,7 @@ public class FeedbackEntity {
     @Column(name = "feedback_id", nullable = false, updatable = false)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.MERGE)
+    @ManyToOne
     @JoinColumn(name = "agent_id")
     private AgentInformationEntity agent;
 
@@ -28,6 +30,14 @@ public class FeedbackEntity {
     @Column(name = "description")
     private String description;
 
+    /**
+     * Constructs a feedback entity with the provided parameters.
+     * 
+     * @param id          The feedback ID.
+     * @param agent       The agent associated with the feedback.
+     * @param title       The title of the feedback.
+     * @param description The description of the feedback.
+     */
     public FeedbackEntity(Long id, AgentInformationEntity agent, String title, String description) {
         this.id = id;
         this.agent = agent;
@@ -35,37 +45,80 @@ public class FeedbackEntity {
         this.description = description;
     }
 
+    /**
+     * Default constructor.
+     */
     public FeedbackEntity() {
     }
 
+    /**
+     * Retrieves the ID of the feedback.
+     * 
+     * @return The feedback ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the feedback.
+     * 
+     * @param id The feedback ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the agent associated with the feedback.
+     * 
+     * @return The agent associated with the feedback.
+     */
     public AgentInformationEntity getAgent() {
         return agent;
     }
 
+    /**
+     * Sets the agent associated with the feedback.
+     * 
+     * @param agent The agent to set.
+     */
     public void setAgent(AgentInformationEntity agent) {
         this.agent = agent;
     }
 
+    /**
+     * Retrieves the title of the feedback.
+     * 
+     * @return The title of the feedback.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the feedback.
+     * 
+     * @param title The title of the feedback to set.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Retrieves the description of the feedback.
+     * 
+     * @return The description of the feedback.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the feedback.
+     * 
+     * @param description The description of the feedback to set.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
