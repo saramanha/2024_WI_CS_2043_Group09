26a003558298c6fc107081aa88dabba164b475b0
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:27:55 -0300

    Update GenderEntity.java

--- src/main/java/com/proj/model/entities/GenderEntity.java
+++ src/main/java/com/proj/model/entities/GenderEntity.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Represents the entity mapping for gender.
+ */
 @Entity
 @Table(name = "gender")
 public class GenderEntity {
@@ -21,35 +24,75 @@ public class GenderEntity {
     @Column(name = "description")
     private String description;
     
+    /**
+     * Constructs a gender entity with the provided parameters.
+     * 
+     * @param id          The gender ID.
+     * @param name        The name of the gender.
+     * @param description The description of the gender.
+     */
     public GenderEntity(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Default constructor.
+     */
     public GenderEntity() {
     }
 
+    /**
+     * Retrieves the ID of the gender.
+     * 
+     * @return The gender ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the gender.
+     * 
+     * @param id The gender ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the name of the gender.
+     * 
+     * @return The name of the gender.
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * Sets the name of the gender.
+     * 
+     * @param name The name of the gender to set.
+     */
     public void setName(String name) {
         this.name = name;
     }
     
+    /**
+     * Retrieves the description of the gender.
+     * 
+     * @return The description of the gender.
+     */
     public String getDescription() {
         return description;
     }
     
+    /**
+     * Sets the description of the gender.
+     * 
+     * @param description The description of the gender to set.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
