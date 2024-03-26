80ddfed3e6048ff275e773d6b08f2cda88ea093c
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:05:48 -0300

    documented BankBranchEntity.java

--- src/main/java/com/proj/model/entities/BankBranchEntity.java
+++ src/main/java/com/proj/model/entities/BankBranchEntity.java
@@ -10,6 +10,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.CascadeType;
 
+/**
+ * Represents a bank branch entity.
+ */
 @Entity
 @Table(name = "bank_branch")
 public class BankBranchEntity {
@@ -25,35 +28,75 @@ public class BankBranchEntity {
     @Column(name = "description")
     private String description;
 
+    /**
+     * Constructs a bank branch entity with provided parameters.
+     * 
+     * @param id          The ID of the bank branch.
+     * @param address     The address of the bank branch.
+     * @param description The description of the bank branch.
+     */
     public BankBranchEntity(Long id, AddressEntity address, String description) {
         this.id = id;
         this.address = address;
         this.description = description;
     }
 
+    /**
+     * Default constructor.
+     */
     public BankBranchEntity() {
     }
 
+    /**
+     * Retrieves the ID of the bank branch.
+     * 
+     * @return The ID of the bank branch.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the bank branch.
+     * 
+     * @param id The ID to set for the bank branch.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the address of the bank branch.
+     * 
+     * @return The address of the bank branch.
+     */
     public AddressEntity getAddress() {
         return address;
     }
 
+    /**
+     * Sets the address of the bank branch.
+     * 
+     * @param address The address to set for the bank branch.
+     */
     public void setAddress(AddressEntity address) {
         this.address = address;
     }
 
+    /**
+     * Retrieves the description of the bank branch.
+     * 
+     * @return The description of the bank branch.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the bank branch.
+     * 
+     * @param description The description to set for the bank branch.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
