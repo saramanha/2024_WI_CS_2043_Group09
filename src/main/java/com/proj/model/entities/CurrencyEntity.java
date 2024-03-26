8dbcf486eda6720f995b520806f11a11a4933082
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:17:56 -0300

    Update CurrencyEntity.java

--- src/main/java/com/proj/model/entities/CurrencyEntity.java
+++ src/main/java/com/proj/model/entities/CurrencyEntity.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Represents the entity mapping for currency details.
+ */
 @Entity
 @Table(name = "currency")
 public class CurrencyEntity {
@@ -21,35 +24,75 @@ public class CurrencyEntity {
     @Column(name = "currency_acronym", nullable = false)
     private String currencyAcronym;
 
+    /**
+     * Constructs a currency entity with the provided parameters.
+     * 
+     * @param id             The currency ID.
+     * @param currencyName   The name of the currency.
+     * @param currencyAcronym The acronym of the currency.
+     */
     public CurrencyEntity(Long id, String currencyName, String currencyAcronym) {
         this.id = id;
         this.currencyName = currencyName;
         this.currencyAcronym = currencyAcronym;
     }
 
+    /**
+     * Default constructor.
+     */
     public CurrencyEntity() {
     }
 
+    /**
+     * Retrieves the currency ID.
+     * 
+     * @return The currency ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the currency ID.
+     * 
+     * @param id The currency ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the name of the currency.
+     * 
+     * @return The name of the currency.
+     */
     public String getCurrencyName() {
         return currencyName;
     }
 
+    /**
+     * Sets the name of the currency.
+     * 
+     * @param currencyName The name of the currency to set.
+     */
     public void setCurrencyName(String currencyName) {
         this.currencyName = currencyName;
     }
 
+    /**
+     * Retrieves the acronym of the currency.
+     * 
+     * @return The acronym of the currency.
+     */
     public String getCurrencyAcronym() {
         return currencyAcronym;
     }
 
+    /**
+     * Sets the acronym of the currency.
+     * 
+     * @param currencyAcronym The acronym of the currency to set.
+     */
     public void setCurrencyAcronym(String currencyAcronym) {
         this.currencyAcronym = currencyAcronym;
     }
