a39de077464f445a8e01dad1cf0d15a1b0d4c04f
linsamman <157435640+linsamman@users.noreply.github.com>
2024-03-26 15:29:46 -0300

    Update RoleEntity.java

--- src/main/java/com/proj/model/entities/RoleEntity.java
+++ src/main/java/com/proj/model/entities/RoleEntity.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Represents the entity mapping for a role.
+ */
 @Entity
 @Table(name = "role")
 public class RoleEntity {
@@ -21,35 +24,75 @@ public class RoleEntity {
     @Column(name = "description")
     private String description;
     
+    /**
+     * Constructs a role entity with the provided parameters.
+     * 
+     * @param id          The role ID.
+     * @param name        The name of the role.
+     * @param description The description of the role.
+     */
     public RoleEntity(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Default constructor.
+     */
     public RoleEntity() {
     }
 
+    /**
+     * Retrieves the ID of the role.
+     * 
+     * @return The role ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the role.
+     * 
+     * @param id The role ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the name of the role.
+     * 
+     * @return The name of the role.
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * Sets the name of the role.
+     * 
+     * @param name The name of the role to set.
+     */
     public void setName(String name) {
         this.name = name;
     }
     
+    /**
+     * Retrieves the description of the role.
+     * 
+     * @return The description of the role.
+     */
     public String getDescription() {
         return description;
     }
     
+    /**
+     * Sets the description of the role.
+     * 
+     * @param description The description of the role to set.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
