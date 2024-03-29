1253944d1023830e67ec0f519987f57d0871908e
Pierre Jaurel Ekotto Mebande <51169483+jaurel69@users.noreply.github.com>
2024-03-29 19:06:10 -0300

    Update ClientSearchService.java
    Implementation

--- src/main/java/com/proj/model/services/ClientSearchService.java
+++ src/main/java/com/proj/model/services/ClientSearchService.java
@@ -1,8 +1,26 @@
 package com.proj.model.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.proj.model.repositories.AgentInformationRepository;
+import com.proj.model.dtos.AgentInformationDTO;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientSearchService {
-     
+    private final AgentInformationRepository agentInformationRepository;
+
+    @Autowired
+    public ClientSearchService(AgentInformationRepository agentInformationRepository) {
+        this.agentInformationRepository = agentInformationRepository;
+    }
+
+    public List<AgentInformationDTO> searchClientsByLastName(String lastName) {
+        List<AgentInformationDTO> clients = agentInformationRepository.findByLastName(lastName)
+                                                .stream()
+                                                .map(agent -> AgentInformationMapper.INSTANCE.entityToDto(agent))
+                                                .collect(Collectors.toList());
+        return clients;
+    }
 }
